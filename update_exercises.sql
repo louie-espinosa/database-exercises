d84c4f53e2e47a72dbfb2d8363d982ad83f612b7
Louie Espinosa <louie.espinosa@proton.me>
2022-08-25 17:18:20 -0500

    Completed mySQL update_exercises

---
+++ update_exercises.sql
@@ -0,0 +1,23 @@
+--All albums
+SELECT * FROM albums
+--All albums released before 1980
+SELECT * FROM albums WHERE release_date < 1980;
+--All albums by Michael Jackson
+SELECT * FROM albums WHERE artist = 'Michael Jackson';
+--Applying update statements:
+--making all albums 10 times more popular
+UPDATE albums
+SET sales = sales * 10
+SELECT * FROM albums
+--move all albums before 1980 to 1800s. YEAR data type will throw an error
+UPDATE albums
+SET release_date = 1800
+WHERE release_date < 1980
+SELECT * FROM albums WHERE release_date < 1980;
+--change 'Michael Jackson' to 'Peter Jackson'
+UPDATE albums
+SET artist = 'Peter Jackson'
+WHERE artist = 'Michael Jackson'
+SELECT * FROM albums WHERE artist = 'Peter Jackson';
+--Add SELECT statements after each update to see update
+
