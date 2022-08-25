fb167ff2867ed337016aadd92243132bd5d3efe0
Louie Espinosa <louie.espinosa@proton.me>
2022-08-25 15:51:27 -0500

    Completed SQL albums_seeder exercise

---
+++ albums_seeder.sql
@@ -0,0 +1,31 @@
+
+
+    --Use INSERT to add records for all the albums from this list on Wikipedia that claim over 30 million sales (the first two tables). For sales data, use the 'sales certification' column of the tables, not 'claimed sales'.
+    INSERT INTO albums (artist, name, release_date, sales, genre)
+    VALUES  ('Michael Jackson', 'Thriller', 1982, 50.2, 'Pop'),
+            ('AC/DC', 'Back in Black', '1982', 30.1, 'Pop'),
+            ('Whitney Houston', 'The Bodyguard', 1992, 28.7, 'R&B/Soul'),
+            ('Pink Floyd', 'The Dark Side of the Moon', 1973, 24.8, 'Progressive Rock'),
+            ('Eagles', 'Their Greatest Hits', 1976, 41.2, 'Country rock'),
+            ('Eagles', 'Hotel California', 1976, 31.8, 'Soft rock'),
+            ('Shania Twain', 'Hotel California', 1976, 31.8, 'Soft rock'),
+            ('Led Zeppelin', 'Led Zeppelin IV', 1971, 30.4, 'Rock'),
+            ('Michael Jackson', 'Bad', 1987, 21.1, 'Pop'),
+            ('Alanis Morissette', 'Jagged Little Pill', 1995, 25.4, 'Rock'),
+            ('Michael Jackson', 'Dangerous', 1991, 16.8, 'Pop'),
+            ('Celine Dion', 'Falling into you', 1996, 21.1, 'Pop'),
+            ('The Beatles', 'Sgt. Pepper''s Lonely Hearts Club Band', 1967, 18.3, 'Rock'),
+            ('Adele', '21', 2011, 27.1, 'Pop'),
+            ('Celine Dion', 'Lets''s Talk About Love', 1997, 19.5, 'Pop'),
+            ('Metallica', 'Metallica', 1991, 22.7, 'Rock'),
+            ('The Beatles', '1', 1991, 23.2, 'Rock'),
+            ('ABBA', 'Gold: Greatest Hits', 1992, 21.6, 'Pop'),
+            ('Bruce Springsteen', 'Born in the U.S.A.', 1984, 22, 'Rock'),
+            ('Madonna', 'The Immaculate Collection', 1990, 19.8, 'Pop'),
+            ('Pink Floyd', 'The Wall', 1979, 18.9, 'Rock'),
+            ('James Horner', 'Titanic: Music from the Motion Picture', 1997, 20, 'Film Score'),
+            ('Dire Straits', 'Brothers in Arms', 1985, 1985, 'Rock'),
+            ('Nirvana', 'Nevermind', 1991, 50, 'Rock'),
+            ('Santana', 'Supernatural', 1999, 20.8, 'Rock'),
+            ('Guns N'' Roses', 'Appetite for Destruction', 1987, 22.8, 'Rock'),
+            ('Elton John', 'Goodbye Yellow Brick Road', 1973, 15.6, 'Pop');
