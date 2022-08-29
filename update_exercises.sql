1ea049c8db9efd66ef8813cdee79dce48d47c84c
Louie Espinosa <louie.espinosa@proton.me>
2022-08-29 10:55:08 -0500

    added comments for sql update_exercises

--- update_exercises.sql
+++ update_exercises.sql
@@ -11,7 +11,7 @@ SET sales = sales * 10
 SELECT * FROM albums
 --move all albums before 1980 to 1800s. YEAR data type will throw an error
 UPDATE albums
-SET release_date = 1800
+SET release_date = 1800 -- or set release_date = release_date - 100
 WHERE release_date < 1980
 SELECT * FROM albums WHERE release_date < 1980;
 --change 'Michael Jackson' to 'Peter Jackson'
