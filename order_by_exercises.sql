a7e55683697d92169037d48776b11d3510bc0e9f
Louie Espinosa <louie.espinosa@proton.me>
2022-08-30 10:59:27 -0500

    Completed order_by exercises

---
+++ order_by_exercises.sql
@@ -0,0 +1,41 @@
+-- describe employees;
+-- Find all employees with first names 'Irena', 'Vidya', or 'Maya' — 709 rows (Hint: Use IN).
+SELECT *
+FROM employees
+WHERE first_name in ('Irena', 'Vidya', 'Maya')
+order by last_name, first_name;
+-- Find all employees whose last name starts with 'E' — 7,330 rows.
+SELECT *
+FROM employees
+WHERE last_name like 'E%'
+order by emp_no desc;
+-- Find all employees with a 'q' in their last name — 1,873 rows.
+SELECT *
+FROM employees
+WHERE last_name like '%q%';
+-- Update your query for 'Irena', 'Vidya', or 'Maya' to use OR instead of IN — 709 rows.
+SELECT *
+FROM employees
+WHERE first_name = 'Irena' or first_name = 'Vidya' or first_name = 'Maya';
+-- Add a condition to the previous query to find everybody with those names who is also male — 441 rows.
+SELECT *
+FROM employees
+where (first_name = 'Irena' or first_name = 'Vidya' or first_name = 'Maya') -- use
+  and gender = 'm';
+-- Find all employees whose last name starts or ends with 'E' — 30,723 rows.
+SELECT *
+FROM employees
+WHERE last_name like 'E%'
+   or last_name like '%E'
+order by emp_no desc;
+-- Duplicate the previous query and update it to find all employees whose last name starts and ends with 'E' — 899 rows.
+SELECT *
+FROM employees
+WHERE last_name like 'E%'
+  and last_name like '%E'
+order by emp_no desc;
+-- Find all employees with a 'q' in their last name but not 'qu' — 547 rows.
+SELECT *
+FROM employees
+WHERE last_name like '%q%'
+  and last_name not like '%qu%';
