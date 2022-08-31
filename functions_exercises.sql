99ce15cdc23b3266e98daa6d0f2279a3c04308f4
Louie Espinosa <louie.espinosa@proton.me>
2022-08-31 14:08:39 -0500

    Completed SQL group_by exercises

--- functions_exercises.sql
+++ functions_exercises.sql
@@ -1,38 +1,34 @@
 
-
-
--- Duplicate the previous query and update it to find all employees whose last name starts and ends with 'E'  899 rows.
--- Update your queries for employees whose names start and end with 'E'. Use concat() to combine their first and last name together as a single column in your results.
+-- 2. Update your queries for employees whose names start and end with 'E'. Use concat() to combine their first and last name together as a single column in your results.
 SELECT concat (first_name, ' ', last_name) as full_name
 FROM employees
 WHERE last_name like 'E%'
   and last_name like '%E'
 ORDER BY first_name;
 
--- Find all employees born on Christmas — 842 rows.
+-- 3. Find all employees born on Christmas — 842 rows.
 select concat (first_name, ' ', last_name) as Born_Christmas_Day
 from employees
 where month(birth_date) = 12
 and day(birth_date) = 25
 order by last_name;
--- Find all employees hired in the 90s and born on Christmas — 362 rows.
+-- 4. Find all employees hired in the 90s and born on Christmas — 362 rows.
 select first_name, last_name, birth_date, hire_date
 from employees
 where month(birth_date) = 12
 and day(birth_date) = 25
 and year(hire_date) between 1990 and 1999;
 
--- Change the query for employees hired in the 90s and born on Christmas such that the first result is the oldest employee who was hired last. It should be Khun Bernini.
+-- 5. Change the query for employees hired in the 90s and born on Christmas such that the first result is the oldest employee who was hired last. It should be Khun Bernini.
 select concat(first_name, ' ', last_name, ' ', birth_date, ' ', hire_date) as old_dog_new_tricks
 from employees
 where month(birth_date) = 12
   and day(birth_date) = 25
   and year(hire_date) between 1990 and 1999
 -- isolate the oldest new hire
-order by birth_date, hire_date desc
-limit 1;
+order by birth_date, hire_date desc;
 
--- For your query of employees born on Christmas and hired in the 90s, use datediff() to find how many days they have been working at the company (Hint: You might also need to use now() or curdate()).
+-- 6. For your query of employees born on Christmas and hired in the 90s, use datediff() to find how many days they have been working at the company (Hint: You might also need to use now() or curdate()).
 select first_name, last_name, birth_date, hire_date,
 -- (a, b) = (a-b), or datediff(expression1, expression2)
     datediff(curdate(), hire_date) as days_since_hired
---
+++ group_by_exercises.sql
@@ -0,0 +1,38 @@
+-- 2. In your script, use DISTINCT to find the unique titles in the titles table.
+select distinct title from titles;
+
+-- 3. Find your query for employees whose last names start and end with 'E'. Update the query to find just the unique last names that start and end with 'E' using GROUP BY
+SELECT last_name
+FROM employees
+WHERE last_name like 'E%'
+  and last_name like '%E'
+group by last_name;
+-- 4. Update your previous query to now find unique combinations of first and last name where the last name starts and ends with 'E'. You should get 846 rows.
+SELECT last_name, first_name
+FROM employees
+WHERE last_name like 'E%'
+  and last_name like '%E'
+group by last_name, first_name;
+
+-- 5. Find the unique last names with a 'q' but not 'qu'.
+SELECT last_name
+FROM employees
+WHERE last_name like '%q%'
+  and last_name not like '%qu%'
+group by last_name;
+
+-- 6. Add a COUNT() to your results and use ORDER BY to make it easier to find employees whose unusual name is shared with others.
+SELECT count(last_name)
+FROM employees
+WHERE last_name like '%q%'
+  and last_name not like '%qu%'
+group by last_name;
+
+-- 7. Update your query for 'Irena', 'Vidya', or 'Maya'. Use count(*) and GROUP BY to find the number of employees for each gender with those names.
+SELECT gender, count(*)
+FROM employees
+WHERE first_name in ('Irena', 'Vidya', 'Maya')
+and gender = 'm'
+or first_name in ('Irena', 'Vidya', 'Maya')
+and gender = 'f'
+group by gender;
