01f29f41d4acba310bd507897b075f5bc6f4b249
Louie Espinosa <louie.espinosa@proton.me>
2022-08-31 11:58:29 -0500

    Completed MySQL functions_exercise

--- limit_exercises.sql
+++ limit_exercises.sql
@@ -1,8 +1,8 @@
 
--- 2.
+-- 2. List the first 10 distinct last name sorted in descending order.
 select distinct last_name
 from employees
-where last_name like 'z%'
+-- where last_name like 'z%' this is redundant
 order by last_name desc
 limit 10;
 
@@ -12,7 +12,7 @@ select emp_no
 from salaries
 order by salary desc
 limit 5;
-
+-- offset is determined by mathing: (a*b-b), or desired page_number * limit number - limit number
 -- Try to think of your results as batches, sets, or pages. The first five results are your first page. The five after that would be your second page, etc. Update the previous query to find the tenth page of results. The employee number results should be:
 select emp_no
 from salaries
